000135d5536ba7fb17d67d4b92ddbd712eb651e8
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-05-13 16:12:34 +0200

    save progress

---
+++ src/main/java/com/planner/travelplanner/domain/dto/hotel/GrossPrice.java
@@ -0,0 +1,20 @@
+package com.planner.travelplanner.domain.dto.hotel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GrossPrice {
+    @JsonProperty("currency")
+    private String currency;
+    @JsonProperty("value")
+    private int value;
+    @JsonProperty("currency")
+    public String getCurrency() {
+        return currency;
+    }
+    @JsonProperty("value")
+    public int getValue() {
+        return value;
+    }
+
+
+}
