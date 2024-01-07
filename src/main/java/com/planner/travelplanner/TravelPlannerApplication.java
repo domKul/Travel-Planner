820360890c052e3bfd88eefa5dd09ba3529245e8
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2024-01-07 19:13:44 +0100

    fix: db auto close fix

--- src/main/java/com/planner/travelplanner/TravelPlannerApplication.java
+++ src/main/java/com/planner/travelplanner/TravelPlannerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-//@OpenAPIDefinition
+//@OpenAPIDefinitio
 public class TravelPlannerApplication {
 
     public static void main(String[] args) {
