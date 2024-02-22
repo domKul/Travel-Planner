5e866002f4de01372ad6245efdf4fb0cd7d0e32a
unknown <domKul>
2024-02-22 18:23:06 +0100

    coplaint entity change

--- src/main/java/travelplanner/complaint/Complaint.java
+++ src/main/java/travelplanner/complaint/Complaint.java
@@ -2,6 +2,9 @@ package travelplanner.complaint;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import travelplanner.complaint.query.SimpleComplaintQueryDto;
 import travelplanner.customer.query.SimpleCustomerQueryDto;
 
@@ -11,7 +14,10 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "complaints")
- class Complaint {
+@Getter
+@Setter
+@EqualsAndHashCode
+class Complaint {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,55 +44,7 @@ import java.util.Objects;
         this.customer.setCustomerId(customerId);
     }
 
-    public Long getComplaintId() {
-        return complaintId;
-    }
-
-    public void setComplaintId(Long complaintId) {
-        this.complaintId = complaintId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getComplaintDate() {
-        return complaintDate;
-    }
-
-    public void setComplaintDate(LocalDateTime complaintDate) {
-        this.complaintDate = complaintDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public SimpleCustomerQueryDto getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(SimpleCustomerQueryDto customer) {
-        this.customer = customer;
-    }
-
-    Complaint mapFromSimpleQueryDto(SimpleComplaintQueryDto simpleComplaintQueryDto){
+    Complaint mapFromSimpleQueryDto(SimpleComplaintQueryDto simpleComplaintQueryDto) {
         return new Complaint(simpleComplaintQueryDto.getComplaintId(),
                 simpleComplaintQueryDto.getTitle(),
                 simpleComplaintQueryDto.getDescription(),
@@ -95,20 +53,8 @@ import java.util.Objects;
                 simpleComplaintQueryDto.getComplaintId()
         );
     }
-    List<Complaint> mapFromSimpleQueryDtoList(List<SimpleComplaintQueryDto> simpleComplaintQueryDto){
+
+    List<Complaint> mapFromSimpleQueryDtoList(List<SimpleComplaintQueryDto> simpleComplaintQueryDto) {
         return simpleComplaintQueryDto.stream().map(this::mapFromSimpleQueryDto).toList();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Complaint that = (Complaint) o;
-        return Objects.equals(complaintId, that.complaintId) && Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(complaintDate, that.complaintDate) && Objects.equals(status, that.status) && Objects.equals(customer, that.customer);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(complaintId, title, description, complaintDate, status, customer);
     }
 }
